74cdd1612daa8489e11ecd819e9506bdeaaf2e08
Igor Montagner <igordsm@gmail.com>
2014-03-17 15:36:47 -0300

    Add google analytics tracking.

--- main.js
+++ main.js
@@ -115,6 +115,7 @@ function changeView(view_id) {
         document.getElementById(view_id).style.display = "block";
         document.title = document.getElementById(view_id).getAttribute("data-title");
     }
+    ga('send', 'pageview', '/' + view_id);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
